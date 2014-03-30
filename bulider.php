760cf56c297f6f5f0e66df7ffbb80af53b1125c1
nada <n.alarfag@gmail.com>
2014-03-30 14:55:19 -0400

    Plugin Description

---
+++ bulider.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+  Plugin Name:  Bulider Plugin 
+  Plugin URI:
+  Description: This plugin create the necessory table for the bulider part
+  Version: 1.0
+  Author: Bulider team
+  Author URI:
+  License: GPLv2
+ */
+
+
+?>

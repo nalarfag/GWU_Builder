0c0f5f84834847116b01ec645c13204d9bbd5245
nada <n.alarfag@gmail.com>
2014-03-30 14:59:50 -0400

    spelling

--- bulider.php
+++ bulider.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name:  Bulider Plugin 
   Plugin URI:
-  Description: This plugin create the necessory table for the bulider part
+  Description: This plugin create the necessory tables for the bulider part
   Version: 1.0
   Author: Bulider team
   Author URI:

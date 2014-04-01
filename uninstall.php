6f48ce13b039990ecec9bc4a3eabd54ab0c792f3
nada <n.alarfag@gmail.com>
2014-03-31 20:27:02 -0400

    format

--- uninstall.php
+++ uninstall.php
@@ -1,39 +1,38 @@
 <?php
+
 // Check that file was called from WordPress admin
-if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit();
+if (!defined('WP_UNINSTALL_PLUGIN'))
+    exit();
 
-bulider_drop_table(  );
+bulider_drop_table();
 
-function bulider_drop_table(  ) {
-	
-     $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'builder', DB_HOST);
-     $builder_db->query("Drop Table Response");
-          $builder_db->query("Drop Table Session");
-     $builder_db->query("Drop Table AnswerChoice");
-     $builder_db->query("Drop Table Action");
-     $builder_db->query("Drop Table Question");
-     $builder_db->query("Drop Table Questionnaire");
+function bulider_drop_table() {
 
-     
-     // Connect to MySQL
+    $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'builder', DB_HOST);
+    $builder_db->query("Drop Table Response");
+    $builder_db->query("Drop Table Session");
+    $builder_db->query("Drop Table AnswerChoice");
+    $builder_db->query("Drop Table Action");
+    $builder_db->query("Drop Table Question");
+    $builder_db->query("Drop Table Questionnaire");
+
+
+    // Connect to MySQL
     $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, TRUE);
     if (!$link) {
         die('Could not connect: ' . mysql_error());
     }
 
-    
-        // Query to drop database 
-        $sql = 'DROP DATABASE builder';
-        $retval = mysql_query($sql, $link);
-        if (!$retval) {
-            die('Could not create database: ' . mysql_error());
-        }
-  
+
+    // Query to drop database 
+    $sql = 'DROP DATABASE builder';
+    $retval = mysql_query($sql, $link);
+    if (!$retval) {
+        die('Could not create database: ' . mysql_error());
+    }
+
 
     mysql_close($link);
-    
 }
 
-   
 ?>

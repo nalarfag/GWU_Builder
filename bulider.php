a6293008bdfde0f34f7a5544104028c4563554a2
nada <n.alarfag@gmail.com>
2014-04-01 01:31:24 -0400

    Shortcode

--- bulider.php
+++ bulider.php
@@ -41,7 +41,7 @@ function builder_activation() {
     builder_create_table();
 }
 
-// Function to create new database table
+// Function to create new database tables
 function builder_create_table() {
 
     $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'builder', DB_HOST);
@@ -125,6 +125,421 @@ function builder_create_table() {
     $builder_db->query($AnswerChoice_creation_query);
     $builder_db->query($Session_creation_query);
     $builder_db->query($Response_creation_query);
+}
+
+//Plugin shortcode 
+// Use label [show_builder_tables] to show data dictionary
+
+add_shortcode('show_builder_tables', function() {
+            return show_builder_tables();
+        });
+
+//builder show tbl function
+function show_builder_tables() {
+    //string to hold the HTML code for output
+    $output = '<body style="padding-top: 58px;">
+		<div id = wrapper>
+			<h1>The Builder Database</h1>';
+
+
+    $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'builder', DB_HOST);
+    // $tables=$builder_db->get_results("SHOW  TABLES FROM  builder");
+    $tables = $builder_db->get_results("select TABLE_NAME from INFORMATION_SCHEMA.TABLES where
+          TABLE_SCHEMA = 'builder' order by create_time");
+
+
+    foreach ($tables as $table) {
+
+        //$tableName=$table->Tables_in_builder;
+        $tableName = $table->TABLE_NAME;
+
+        $columns = $builder_db->get_results("SHOW COLUMNS FROM " . $tableName . " ");
+
+
+        $output .=' <br><div class=table>
+            <h2>' . $tableName . '</h2>
+            <br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+                                        <tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Primary</th>
+						</tr>';
+        foreach ($columns as $column) {
+            $columnName = $column->Field;
+            $columnType = $column->Type;
+            $columnNull = $column->Null;
+            if (strcmp($column->Key, 'PRI') == 0) {
+                $columnPrimary = 'yes';
+            } else {
+                $columnPrimary = '';
+            }
+
+            $output .= '  <tr>
+				<td nowrap="nowrap">' . $columnName . '</td>
+				<td xml:lang="en" dir="ltr" nowrap="nowrap">' . $columnType . '</td>
+				<td>' . $columnNull . '</td>
+				<td nowrap="nowrap">' . $columnPrimary . '</td>
+		    </tr>';
+        }
+        $output.='		</tbody>
+				</table>
+			</div>';
+    }
+
+    return $output;
+}
+
+//Plugin shortcode 
+// Use label [builder_show_tables] to show data dictionary
+
+add_shortcode('builder_show_tables', function() {
+            return builder_show_tbl();
+        });
+
+//builder show tbl function
+function builder_show_tbl() {
+    $table_html =
+            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html lang="en" dir="ltr" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+	<body style="padding-top: 58px;">
+		<div id = wrapper>
+			<h1>The Builder Database</h1>
+			<div class=table>
+				<h2>Action</h2>
+				Table comments: action
+				<br>
+				<br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+						<tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Default</th>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> ID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> QuestionnaireID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Question_Number </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Sequence </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd marked">
+							<td nowrap="nowrap"> Type </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Link </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(200)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Content </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(255)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Duration </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(10)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class=table>
+				<h2>Answerchoice</h2>
+				Table comments: answerchoice
+				<br>
+				<br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+						<tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Default</th>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> OptionNumber </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> QuestionnaireID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Question_Number </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Value </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(255)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class=table>
+				<h2>Question</h2>
+				Table comments: question
+				<br>
+				<br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+						<tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Default</th>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Question_Number </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap">QuestionnaireID</td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Type </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Text </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(255)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Mandatory </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">tinyint(1)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class=table>
+				<h2>Questionnaire</h2>
+				Table comments: questionnaire
+				<br>
+				<br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+						<tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Default</th>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> ID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(20)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Title </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Date_created </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">date</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Topic </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Anonymous </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">tinyint(1)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Multiple </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">tinyint(1)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> creator_name </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class=table>
+			   <h2>Response</h2>
+				Table comments: response
+				<br>
+				<br>
+				<table class="print" width="100%" border="1">
+					<tbody>
+						<tr>
+							<th width="50">Column</th>
+							<th width="80">Type</th>
+							<th width="40">Null</th>
+							<th width="70">Default</th>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> ID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> SessionID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> QuestionnaireID </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Question_Number </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>No</td>
+							<td nowrap="nowrap"></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> AnswerNumber </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> TextTyped </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">text</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="odd">
+							<td nowrap="nowrap"> Link </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(255)</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+						<tr class="even">
+							<td nowrap="nowrap"> Action </td>
+							<td xml:lang="en" dir="ltr" nowrap="nowrap">text</td>
+							<td>Yes</td>
+							<td nowrap="nowrap"><i>NULL</i></td>
+						</tr>
+					</tbody>
+				</table>
+				<div class=table>
+					<h2>Session</h2>
+					Table comments: session
+					<br>
+					<br>
+					<table class="print" width="100%" border="1">
+						<tbody>
+							<tr>
+								<th width="50">Column</th>
+								<th width="80">Type</th>
+								<th width="40">Null</th>
+								<th width="70">Default</th>
+							</tr>
+							<tr class="odd">
+								<td nowrap="nowrap"> ID </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">int(11)</td>
+								<td>No</td>
+								<td nowrap="nowrap"></td>
+							</tr>
+							<tr class="even">
+								<td nowrap="nowrap"> User_name </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+								<td>Yes</td>
+								<td nowrap="nowrap"><i>NULL</i></td>
+							</tr>
+							<tr class="odd">
+								<td nowrap="nowrap"> Completed </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">tinyint(1)</td>
+								<td>No</td>
+								<td nowrap="nowrap"></td>
+							</tr>
+							<tr class="even">
+								<td nowrap="nowrap"> Duration </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">time</td>
+								<td>No</td>
+								<td nowrap="nowrap"></td>
+							</tr>
+							<tr class="odd">
+								<td nowrap="nowrap"> Date </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">date</td>
+								<td>No</td>
+								<td nowrap="nowrap"></td>
+							</tr>
+							<tr class="even">
+								<td nowrap="nowrap"> IP </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(20)</td>
+								<td>No</td>
+								<td nowrap="nowrap"></td>
+							</tr>
+							<tr class="odd">
+								<td nowrap="nowrap"> Location </td>
+								<td xml:lang="en" dir="ltr" nowrap="nowrap">varchar(100)</td>
+								<td>Yes</td>
+								<td nowrap="nowrap"><i>NULL</i></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+	</body>
+</html> ';
+    echo $table_html;
 }
 
 ?>

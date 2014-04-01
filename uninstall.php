632989c7b93d5b175ff3bd7fc84b4ca7a3f4601b
nada <n.alarfag@gmail.com>
2014-03-31 20:24:44 -0400

    add drop statements

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,39 @@
 <?php
+// Check that file was called from WordPress admin
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+	exit();
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+bulider_drop_table(  );
+
+function bulider_drop_table(  ) {
+	
+     $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'builder', DB_HOST);
+     $builder_db->query("Drop Table Response");
+          $builder_db->query("Drop Table Session");
+     $builder_db->query("Drop Table AnswerChoice");
+     $builder_db->query("Drop Table Action");
+     $builder_db->query("Drop Table Question");
+     $builder_db->query("Drop Table Questionnaire");
+
+     
+     // Connect to MySQL
+    $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, TRUE);
+    if (!$link) {
+        die('Could not connect: ' . mysql_error());
+    }
+
+    
+        // Query to drop database 
+        $sql = 'DROP DATABASE builder';
+        $retval = mysql_query($sql, $link);
+        if (!$retval) {
+            die('Could not create database: ' . mysql_error());
+        }
+  
+
+    mysql_close($link);
+    
+}
+
+   
 ?>

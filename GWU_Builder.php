3ac51a481c11082f80667acafcb8272ea66365ee
nada <n.alarfag@gmail.com>
2014-04-04 19:42:33 -0400

    Modify SQL to reflect the latest data model

--- GWU_Builder.php
+++ GWU_Builder.php
@@ -16,28 +16,7 @@ register_activation_hook(__FILE__, 'builder_activation');
 // Activation Callback
 function builder_activation() {
 
-/*
-    // Connect to MySQL
-    $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, TRUE);
-    if (!$link) {
-        die('Could not connect: ' . mysql_error());
-    }
 
-    // Make builder the current database
-    $db_selected = mysql_select_db('GWU_Builder', $link);
-
-    if (!$db_selected) {
-        // If we couldn't, then it either doesn't exist, or we can't see it.
-        $sql = 'CREATE DATABASE GWU_Builder';
-
-        $retval = mysql_query($sql, $link);
-        if (!$retval) {
-            die('Could not create database: ' . mysql_error());
-        }
-    }
-
-    mysql_close($link);
-*/
     // Create table on main blog in network mode or single blog
     builder_create_table();
 }
@@ -52,37 +31,37 @@ function builder_create_table() {
     // using received table prefix
     $Questionnaire_creation_query =
             'CREATE TABLE IF NOT EXISTS  GWU_Questionnaire (
-                `ID` INT(20) NOT NULL AUTO_INCREMENT ,
+                `QuestionnaireID` INT(20) NOT NULL AUTO_INCREMENT ,
                 `Title` VARCHAR( 100 ) NOT NULL ,
-                `Date_created` DATE NOT NULL ,
+                `DateCreated` DATE NOT NULL ,
                 `Topic` VARCHAR( 100 ) NULL ,
-                `Anonymous` BOOL NOT NULL ,
-                `Multiple` BOOL NULL ,
-                `creator_name` VARCHAR( 100 ) NOT NULL,
-                PRIMARY KEY (`ID`)
+                `AllowAnonymous` BOOL NOT NULL ,
+                `AllowMultiple` BOOL NULL ,
+                `CreatorName` VARCHAR( 100 ) NOT NULL,
+                PRIMARY KEY (`QuestionnaireID`)
             )ENGINE = INNODB;';
     $Question_creation_query =
             'CREATE TABLE IF NOT EXISTS  GWU_Question (
                 `Question_Number` INT NOT NULL ,
                 `QuestionnaireID` INT NOT NULL ,
-                `Type` VARCHAR( 100 ) NOT NULL ,
+                `AnsType` VARCHAR( 100 ) NOT NULL ,
                 `Text` VARCHAR( 255 ) NOT NULL ,
                 `Mandatory` BOOL NOT NULL ,
                 PRIMARY KEY (  `Question_Number` , `QuestionnaireID` ),
-                FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Questionnaire(`ID`)
+                FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Questionnaire(`QuestionnaireID`)
             )ENGINE = INNODB;';
 
     $Action_creation_query =
             'CREATE TABLE IF NOT EXISTS  GWU_Action (
-                `ID` INT( 11 ) NOT NULL ,
+                `ActionID` INT( 11 ) NOT NULL ,
                 `QuestionnaireID` INT NOT NULL ,
                 `Question_Number` INT NOT NULL ,
                 `Sequence` INT NOT NULL ,
-                `Type` VARCHAR( 100 ) NOT NULL ,
-                `Link` VARCHAR( 200 ) NULL ,
+                `ActionType` VARCHAR( 100 ) NOT NULL ,
+                `LinkToAction` VARCHAR( 200 ) NULL ,
                 `Content` VARCHAR( 255 ) NULL ,
                 `Duration` INT( 10 ) NOT NULL ,
-                PRIMARY KEY (  `ID` ,  `QuestionnaireID` ,  `Question_Number` ),
+                PRIMARY KEY (  `ActionID` ,  `QuestionnaireID` ,  `Question_Number` ),
                 FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Question(`QuestionnaireID`),
                 FOREIGN KEY (`Question_Number`) REFERENCES GWU_Question(`Question_Number`)
             ) ENGINE = INNODB;';
@@ -91,34 +70,65 @@ function builder_create_table() {
                 `OptionNumber` INT NOT NULL ,
                 `QuestionnaireID` INT NOT NULL ,
                 `Question_Number` INT NOT NULL ,
-                `Value` VARCHAR( 255 ) NULL ,
+                `AnsValue` VARCHAR( 255 ) NULL ,
                 PRIMARY KEY (  `OptionNumber` ,  `QuestionnaireID` ,  `Question_Number` ),
                 FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Question(`QuestionnaireID`),
                 FOREIGN KEY (`Question_Number`) REFERENCES GWU_Question(`Question_Number`)
             ) ENGINE = INNODB;';
+     $Flag_creation_query =
+            'CREATE TABLE IF NOT EXISTS  GWU_Flag (
+                `FlagID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                 `FlagName` VARCHAR( 100 ) NOT NULL ,
+                `FlagValue` VARCHAR( 100 ) NOT NULL
+            ) ENGINE = INNODB;';
+       $FlagSet_creation_query =
+            'CREATE TABLE IF NOT EXISTS  GWU_FlagSet (
+                `FlagID` INT,
+                `QuestionnaireID` INT NOT NULL ,
+                `Question_Number` INT NOT NULL ,
+                `OptionNumber` INT NULL ,
+                PRIMARY KEY (   `FlagID`,`OptionNumber` ,  `QuestionnaireID` ,  `Question_Number` ),
+                 FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_AnswerChoice(`QuestionnaireID`),
+                FOREIGN KEY (`Question_Number`) REFERENCES GWU_AnswerChoice(`Question_Number`),
+                FOREIGN KEY (`OptionNumber`) REFERENCES GWU_AnswerChoice(`OptionNumber`),
+                FOREIGN KEY (`FlagID`) REFERENCES GWU_Flag(`FlagID`)
+            ) ENGINE = INNODB;';
+             $FlagCheck_creation_query =
+            'CREATE TABLE IF NOT EXISTS  GWU_FlagCheck (
+                `FlagID` INT,
+                `QuestionnaireID` INT NOT NULL ,
+                `Question_Number` INT NOT NULL ,
+                PRIMARY KEY (   `FlagID`,  `QuestionnaireID` ,  `Question_Number` ),
+                 FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Question(`QuestionnaireID`),
+                FOREIGN KEY (`Question_Number`) REFERENCES GWU_Question(`Question_Number`),
+                FOREIGN KEY (`FlagID`) REFERENCES GWU_Flag(`FlagID`)
+            ) ENGINE = INNODB;';
     $Session_creation_query =
             'CREATE TABLE IF NOT EXISTS  GWU_Session (
-                `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                `SessionID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
                 `User_name` VARCHAR( 100 ) NULL ,
-                `Completed` BOOL NOT NULL ,
+                `SurveyCompleted` BOOL NOT NULL ,
                 `Duration` TIME NOT NULL ,
-                `Date` DATE NOT NULL ,
+                `SurveyTakenDate` DATE NOT NULL ,
                 `IP` VARCHAR( 20 ) NOT NULL ,
-                `Location` VARCHAR( 100 ) NULL
+                `City` VARCHAR( 50 ) NULL,
+                `Country` VARCHAR( 50 ) NULL
             ) ENGINE = INNODB;';
     $Response_creation_query =
             'CREATE TABLE IF NOT EXISTS  GWU_Response (
-                `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                `ResponceID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
                 `SessionID` INT NOT NULL ,
                 `QuestionnaireID` INT NOT NULL ,
                 `Question_Number` INT NOT NULL ,
                 `AnswerNumber` INT NULL ,
-                `TextTyped` TEXT NULL ,
-                `Link` VARCHAR( 255 ) NULL ,
-                `Action` TEXT NULL,
+                `ResponceType` VARCHAR( 100 ) NULL ,
+                `ResponceContent` TEXT NULL ,
+                `CodeToProcessResponce` TEXT NULL ,
+                `ProcessingResult` TEXT NULL,
                 FOREIGN KEY (`QuestionnaireID`) REFERENCES GWU_Question(`QuestionnaireID`),
                 FOREIGN KEY (`Question_Number`) REFERENCES GWU_Question(`Question_Number`),
-                FOREIGN KEY (`AnswerNumber`) REFERENCES GWU_AnswerChoice(`OptionNumber`)
+                FOREIGN KEY (`AnswerNumber`) REFERENCES GWU_AnswerChoice(`OptionNumber`),
+                FOREIGN KEY (`SessionID`) REFERENCES GWU_Session(`SessionID`)
             ) ENGINE = INNODB;';
 
    // $builder_db->query($Questionnaire_creation_query);
@@ -132,6 +142,9 @@ function builder_create_table() {
     $wpdb->query($Question_creation_query);
     $wpdb->query($Action_creation_query);
     $wpdb->query($AnswerChoice_creation_query);
+    $wpdb->query($Flag_creation_query);
+    $wpdb->query($FlagSet_creation_query);
+    $wpdb->query($FlagCheck_creation_query);
     $wpdb->query($Session_creation_query);
     $wpdb->query($Response_creation_query);
 }
--- uninstall.php
+++ uninstall.php
@@ -8,36 +8,14 @@ bulider_drop_table();
 
 function bulider_drop_table() {
 
-   /* $builder_db = new wpdb(DB_USER, DB_PASSWORD, 'GWU_Builder', DB_HOST);
-    $builder_db->query("Drop Table Response");
-    $builder_db->query("Drop Table Session");
-    $builder_db->query("Drop Table AnswerChoice");
-    $builder_db->query("Drop Table Action");
-    $builder_db->query("Drop Table Question");
-    $builder_db->query("Drop Table Questionnaire");
-
-
-    // Connect to MySQL
-    $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD, TRUE);
-    if (!$link) {
-        die('Could not connect: ' . mysql_error());
-    }
-
-
-    // Query to drop database 
-    $sql = 'DROP DATABASE GWU_Builder';
-    $retval = mysql_query($sql, $link);
-    if (!$retval) {
-        die('Could not create database: ' . mysql_error());
-    }
-
-
-    mysql_close($link);
-*/
+ 
 
 global $wpdb;
     $wpdb->query("Drop Table GWU_Response");
     $wpdb->query("Drop Table GWU_Session");
+    $wpdb->query("Drop Table GWU_FlagCheck");
+    $wpdb->query("Drop Table GWU_FlagSet");
+    $wpdb->query("Drop Table GWU_Flag");
     $wpdb->query("Drop Table GWU_AnswerChoice");
     $wpdb->query("Drop Table GWU_Action");
     $wpdb->query("Drop Table GWU_Question");

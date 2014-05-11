4af176ea7b292d47bf565a18e987b89cb86a6170
nada <n.alarfag@gmail.com>
2014-05-10 21:36:35 -0400

    Fix survey view

--- views/QuestionnaireViewAdmin.php
+++ views/QuestionnaireViewAdmin.php
@@ -1,189 +1,224 @@
 <script type='text/javascript'>
 
+        
+    jQuery( document ).ready( function($) {  
+        $(this).find(".button-primary").hide();
+        $(document).on('mouseenter', '.divbutton', function () {
+            $(this).find(".button-primary").show();
+        }).on('mouseleave', '.divbutton', function () {
+            $(this).find(".button-primary").hide();
+        });
+    });
+            
+    jQuery( document ).ready( function($) { 
+        var id =1;
+        var ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>';
+        $("#dialog-confirm-multiple").dialog({
+            dialogClass   : "wp-dialog",
+            autoOpen: false,
+            resizable: false,
+            width: 300,
+            modal: true,
+            buttons: {
+                "Yes": function () { 
+		   
+                    $(this).dialog("close");
+                    $.ajax({
+                        type: "POST",
+                        url: ajax_url,
+                        action: 'delete_questionnaire',
+                        data:
+                            {
+                            action: 'delete_questionnaire',
+                            id:id
+                        }
+                        ,
+                        success: function(data) {
+                            //   window.location.reload(true);
+                            // $("#"+data).remove();
+                            $("#QuestionnaireView").load(location.href + " #QuestionnaireView");
+                            $("#Notice").html('<div class="updated"><p>The questionnaire was successfully deleted</p></div>');
 
-  jQuery( document ).ready( function($) {
-  $(this).find(".button-primary").hide();
-  $(document).on('mouseenter', '.divbutton', function () {
-  $(this).find(".button-primary").show();
-  }).on('mouseleave', '.divbutton', function () {
-  $(this).find(".button-primary").hide();
-  });
-  });
+                        }
 
-  jQuery( document ).ready( function($) {
-  var id =1;
-  $("#dialog-success").dialog({
-  dialogClass   : "wp-dialog",
-  autoOpen: false,
-  resizable: false,
-  width: 300,
-  modal: true,
-  buttons: {
-  "OK": function () {
-  $(this).dialog("close");
-  }
-  }
-  });
-  $("#dialog-confirm-multiple").dialog({
-  dialogClass   : "wp-dialog",
-  autoOpen: false,
-  resizable: false,
-  width: 300,
-  modal: true,
-  buttons: {
-  "Yes": function () {
-  var ajax_url = '<?php echo admin_url( 'admin-ajax.php' ); ?>';
-  var val=$('#view_question').serialize() ;
-  $(this).dialog("close");
-  $.ajax({
-  type: "POST",
-  url: ajax_url,
-  action: 'delete_question',
-  data:
-  {
-  action: 'delete_question',
-  value: val,
-  id:id
-  }
-  ,
-  success: function(data) {
-  // id = $(this).closest().attr("id");
-  $("#dialog-success").dialog('open');
-  // $("#"+data).remove();
-  $("#QuestionView").load(location.href + " #QuestionView");
-  }
+                    });
 
-  });
-
-  },
-  "No": function () {
-  $(this).dialog("close");
-  }
-  }
-  });
-  $(document).on("click","#delete",function(e,ui){
-  //$("#delete").click(function (e,ui) {
-  //   debugger;
-  if(e.originalEvent) {
-  e.preventDefault();
-  id = $(this).closest('div').parent().attr('id');
-  //   console.debug(id);
-  $("#dialog-confirm-multiple").dialog('open');
-  return false;
-  }
-  });
-  });
-
-</script>
-<div id="QuestionView">
-
-
-  <?php
-$adminURL= admin_url('admin-post.php');
- foreach ($questions as $question) {
-                $Title = $question->get_Text();
-                $type = $question->get_AnsType();
-                $questionno = $question->get_QuestionNumber();
-                $QuestionSeq=$question->get_QuestSequence();
-		$Mandatory= ($question->get_Mandatory()==1 ?'*' : '' );
-
-                ?>
-  <div id="question_"<?php echo $QuestionnaireID; ?>_<?php echo $QuestionSeq; ?>" class="wrap">
-  <div class="divbutton" >
-    <form id="view_question"  method="post" action=""<?php echo $adminURL; ?>">
-    <input type="hidden" name="action" value="question_handler" />
-    <input type="hidden" name="QuestionnaireID" value=""<?php echo $QuestionnaireID; ?>" />
-    <input type="hidden" name="QuestionSeq" value=""<?php echo $QuestionSeq; ?>" />
-
-
-
-    <table>
-      <?php if($PublishedFlag!=1) {?>
-      <tr>
-        <th colspan="100%" align="left">
-
-          <input type="submit" name="add" value="Add" class="button-primary"/>
-          <input type="submit" name="edit" value="Edit" class="button-primary"/>
-          <input type="submit" name="logic" value="Logic" class="button-primary"/>
-          <input type="submit" name="addAction" value="Action" class="button-primary"/>
-          <input type="submit" id="delete" name="delete" value="Delete" class="button-primary"/>
-        </th>
-      </tr>
-      <?php }?>
-      <tr>
-        <th colspan="100%" align="left">
-          <?php echo $Mandatory;
-		   echo $questionno; ?> &nbsp;&nbsp;&nbsp; <?php echo $Title; ?>
-        </th>
-      </tr>
-      <?php
-                $answerchoices =  $this->Wrapper->listAnswerChoice($QuestionnaireID, $QuestionSeq);
-
-                if ($type == 'Text Box') {
-                      echo '  <input type="hidden" name="QuestioType" value="essay" />';
-                  echo '
-                 <tr>
-                <td class="style1">
-                <textarea  cols="30" rows="5"> </textarea></td>
-                </tr>';
-                } elseif ($type == 'NPS') {
-                  echo '  <input type="hidden" name="QuestioType" value="NPS" />';
-		   echo '<tr>';
-                    for ($i = 0; $i <= 10; $i++) {
-                        echo  '<td><input name="' . $questionno . '" type="radio"
-                            value="' . $answerchoices[$i]->get_OptionNumber() . '"/>&nbsp;</td>';
-                    }
-		   echo '</tr>
-		       <tr>';
-                    for ($i = 0; $i <= 10; $i++) {
-                        echo '<td>' . $answerchoices[$i]->get_AnsValue() . '</td>';
-                    }
-		   echo '</tr>';
-		   echo '<tr>
-		       <td colspan="6" align="left">'.$answerchoices[11]->get_AnsValue().'</td>
-		       <td colspan="5" align="right">'.$answerchoices[12]->get_AnsValue().'</td>
-
-		       </tr>';
-                } elseif ($type == 'Multiple Choice, Single Value') {
-                   echo '  <input type="hidden" name="QuestioType" value="multipleS" />';
-                    foreach ($answerchoices as $answerchoice) {
-                        $answerchoicescontent = $answerchoice->get_AnsValue();
-
-                       echo '
-                          <tr>  <td class="style1">
-                          <input name="' . $questionno . '" type="radio" value="' . $answerchoice->get_OptionNumber() . '"/> 
-                           &nbsp;&nbsp;' . $answerchoicescontent . '</td></tr>';
-                    }
-                    
-                } else {
-                      echo '  <input type="hidden" name="QuestioType" value="multipleM" />';
-                    foreach ($answerchoices as $answerchoice) {
-                        $answerchoicescontent = $answerchoice->get_AnsValue();
-
-                       echo ' <tr>  <td class="style1">
-                           <input name="' . $questionno . '" value="' . $answerchoice->get_OptionNumber() . '" type="checkbox"/>
-                               &nbsp;&nbsp;' . $answerchoicescontent . '</td></tr>';
-                    }
+                },
+                "No": function () {
+                    $(this).dialog("close");
                 }
-		echo '</table> </form> </div><hr/></div>';
             }
-	      if($PublishedFlag!=1) {
-                echo' <h2>    <a class="add-new-h2" 
-			href="' . add_query_arg(
-                    array('page' => 'GWU_add-Questionnaire-page',
-                'id' => 'new', 'Qid' => $QuestionnaireID,
-                'type' => 'multipleS'), admin_url('admin.php'))
-            . '">Add New Question</a></h2>';
-	      }
+        });
+        $(document).on("click","a#delete",function(e,ui){
+            //   debugger;
+            if(e.originalEvent) {
+                e.preventDefault();
+                var url = ($(this).attr('href'));
+                id = getURLParameter(url, 'qid');
+                //  console.debug(id);
+                $("#dialog-confirm-multiple").dialog('open');
+                return false;
+            }
+        });
+        
+        $(document).on("click","a#publish",function(e,ui){
 
-           
-?>
-    </div>
+            e.preventDefault();
+            var url = ($(this).attr('href'));
+            id = getURLParameter(url, 'qid');
+            //   console.debug(id);
+            //   debugger;
+            $.ajax({
+                type: "POST",
+                url: ajax_url,
+                action: 'publish_questionnaire',
+                data:
+                    {
+                    action: 'publish_questionnaire',
+                    id:id
+                }
+                ,
+                success: function(data) {
+                    //   window.location.reload(true);
+                    // $("#"+data).remove();
+		    var response=$(data);
+		    var success=response.filter('#success').text();
 
-  <div id="dialog-confirm-multiple" title="Confirmation Required">
+                    $("#QuestionnaireView").load(location.href + " #QuestionnaireView");
+		    if(success=='true')
+                        $("#Notice").html('<div class="updated"><p>The questionnaire was successfully published</p></div>');
+                    else
+                        $("#Notice").html('<div class="updated"><p>Faild to publish the questionnaire</p></div>');
+
+                }
+
+            });
+            
+        });
+        $(document).on("click","a#deactivate",function(e,ui){
+
+            e.preventDefault();
+            var url = ($(this).attr('href'));
+            id = getURLParameter(url, 'qid');
+            //   console.debug(id);
+            //   debugger;
+            $.ajax({
+                type: "POST",
+                url: ajax_url,
+                action: 'deactivate_questionnaire',
+                data:
+                    {
+                    action: 'deactivate_questionnaire',
+                    id:id
+                }
+                ,
+                success: function(data) {
+                    //   window.location.reload(true);
+                    // $("#"+data).remove();
+		    var response=$(data);
+		    var success=response.filter('#success').text();
+		    //console.debug(success);
+                    $("#QuestionnaireView").load(location.href + " #QuestionnaireView");
+		    if(success=='true')
+                        $("#Notice").html('<div class="updated"><p>The questionnaire link was successfully deactivated</p></div>');
+                    else
+                        $("#Notice").html('<div class="updated"><p>Faild to deactivate the questionnaire link</p></div>');
+
+                }
+
+            });
+            
+        });
+        $(document).on("click","a#reactivate",function(e,ui){
+
+            e.preventDefault();
+            var url = ($(this).attr('href'));
+            id = getURLParameter(url, 'qid');
+            //   console.debug(id);
+            //   debugger;
+            $.ajax({
+                type: "POST",
+                url: ajax_url,
+                action: 'reactivate_questionnaire',
+                data:
+                    {
+                    action: 'reactivate_questionnaire',
+                    id:id
+                }
+                ,
+                success: function(data) {
+                    //   window.location.reload(true);
+                    // $("#"+data).remove();
+		    var response=$(data);
+		    var success=response.filter('#success').text();
+		    //console.debug(success);
+                    $("#QuestionnaireView").load(location.href + " #QuestionnaireView");
+		    if(success=='true')
+                        $("#Notice").html('<div class="updated"><p>The questionnaire was successfully republished</p></div>');
+                    else
+			$("#Notice").html('<div class="updated"><p>Failed to republished the questionnaire link</p></div>');
+
+                }
+
+            });
+            
+        });
+        $(document).on("click","a#duplicate",function(e,ui){
+
+            e.preventDefault();
+            var url = ($(this).attr('href'));
+            id = getURLParameter(url, 'qid');
+	    // console.debug(id);
+            debugger;
+            $.ajax({
+                type: "POST",
+                url: ajax_url,
+                action: 'duplicate_questionnaire',
+                data:
+                    {
+                    action: 'duplicate_questionnaire',
+                    id:id
+                }
+                ,
+                success: function(data) {
+                    //   window.location.reload(true);
+                    // $("#"+data).remove();
+                    $("#QuestionnaireView").load(location.href + " #QuestionnaireView");
+                    $("#Notice").html('<div class="updated"><p>The questionnaire was successfully duplicated</p></div>');
+                }
+
+            });
+            
+        });
+    });
+    function getURLParameter(url, name) {
+        return (RegExp(name + '=' + '(.+?)(&|$)').exec(url)||[,null])[1];
+    }    
+</script>
+<?php
+$NewQuestionnaireAddress = add_query_arg(array(
+    'page' => 'GWU_add-Questionnaire-page',
+    'id' => 'newQuestionnaire'
+        ), admin_url('admin.php'));
+?>          
+
+<div  class="wrap">
+    <h2>Survey <a class="add-new-h2"  href="<?php echo $NewQuestionnaireAddress; ?>">Add new</a></h2>
+
+    <div id="Notice"></div>
+
+    <div id="QuestionnaireView">
+        <?php
+        //show the Table of Questionnaire
+        $wp_list_table = new Questionnaire_List_Table();
+        $wp_list_table->prepare_items();
+        $wp_list_table->display();
+        ?>
+    </div> </div>
+
+
+
+<div id="dialog-confirm-multiple" title="Confirmation Required">
     <p>This item will be permanently deleted and cannot be recovered. Are you sure?</p>
-  </div>
+</div>
 
-  <div id="dialog-success" title="Item deleted">
-    <p>The item was successfully deleted</p>
-  </div>
